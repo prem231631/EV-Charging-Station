8ee1dbe6214825004ef68c63b02270cdac50ed94
Prem Raj Chaudhary <prem.231631@ncit.edu.np>
2026-09-01 12:55:56 +0545

    All the Components are imported into Home Page

--- frontend/src/pages/public/Home.jsx
+++ frontend/src/pages/public/Home.jsx
@@ -1,9 +1,28 @@
+import Navbar from "../../components/home/Navbar";
+import Hero from "../../components/home/Hero";
+import HowItWorks from "../../components/home/HowItWorks";
+import Features from "../../components/home/Features";
+import PopularStations from "../../components/home/PopularStations";
+import WhyChooseUs from "../../components/home/WhyChooseUs";
+import CTA from "../../components/home/CTA";
+import Footer from "../../components/home/Footer";
+
 function Home() {
     return (
-        <div>
-            <h1>EV Charging Station</h1>
-            <p>Welcome to the EV Charging Station platform.</p>
-        </div>
+        <>
+            <Navbar />
+
+            <main>
+                <Hero />
+                <HowItWorks />
+                <Features />
+                <PopularStations />
+                <WhyChooseUs />
+                <CTA />
+            </main>
+
+            <Footer />
+        </>
     );
 }
 
