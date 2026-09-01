5007a43e781561c431917429035d271925b828f1
Prem Raj Chaudhary <prem.231631@ncit.edu.np>
2026-09-01 14:00:45 +0545

    Creating Application Configuration

---
+++ backend/app/core/config.py
@@ -0,0 +1,16 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+
+    database_url: str
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        case_sensitive=False,
+        extra="ignore",
+    )
+
+
+settings = Settings()
