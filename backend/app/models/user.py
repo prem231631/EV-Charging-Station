242b130581bfd48a584d7d4a0824dd90594d7970
Prem Raj Chaudhary <prem.231631@ncit.edu.np>
2026-09-03 20:20:06 +0545

    user model updated for vehicle model

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -4,7 +4,10 @@ from sqlalchemy import Boolean, DateTime, String, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.database import Base
+from typing import TYPE_CHECKING
 
+if TYPE_CHECKING:
+    from app.models.vehicle import Vehicle
 
 class User(Base):
     __tablename__ = "users"
