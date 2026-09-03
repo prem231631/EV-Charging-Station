3d0e768b00540e101178b8cccd9c833b892eb61a
Prem Raj Chaudhary <prem.231631@ncit.edu.np>
2026-09-03 19:42:15 +0545

    Exporting booking model

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,6 +2,7 @@ from app.models.user import User
 from app.models.vehicle import Vehicle
 from app.models.station import Station
 from app.models.charger import Charger
+from app.models.booking import Booking
 
 __all__ = [
     "User",
